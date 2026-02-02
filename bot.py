75daf92a5bf1fb0980e801cfeeb7475c6ab9874f
msndoeduardo-ctrl <msn.do.eduardo@gmail.com>
2026-02-02 20:20:52 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 import discord
 from discord.ext import commands
+from flask import Flask
+from threading import Thread
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -16,5 +15,21 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f"🤖 Bot conectado como {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong! Online no Railway!")
+
+# Flask pra manter vivo
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot Discord rodando!"
+
+def run_flask():
+    app.run(host="0.0.0.0", port=8080)
+
+Thread(target=run_flask).start()
 
 bot.run(TOKEN)
